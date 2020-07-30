3fe3ed7e7a2dc124fcb82ba54173fc9bfdc417ae
condi-av <68968499+condi-av@users.noreply.github.com>
2020-07-31 01:58:20 +0300

    Add files via upload

---
+++ res/x5settings.js
@@ -0,0 +1,112 @@
+(function ( $, x5engine ) {
+	var x = x5engine,
+		s = x.settings,
+		p = s.currentPath,
+		b = x.boot;
+
+	s.siteId = 'CFA6EBAE6FFD31852F2A9AC7E0696A12';
+	s.version = '12-0-6-24';
+	b.push(function () {
+		x.setupDateTime();
+		x.imAccess.showLogout();
+		x.utils.autoHeight();
+		x.cart.ui.updateWidget();
+		x.imGrid.init();
+	});
+	s.menu = {
+		verticalScroll: false,
+		orientation: 'horizontal'
+	};
+	b.push(function () {
+		x.menu({
+			target: '#imMnMn',
+			showCurrent: false,
+			desktopVerticalScroll: false,
+			mobileVerticalScroll: false,
+			showLogoOnScroll: false,
+			orientation: 'horizontal',
+			menuHeight: 65,
+			menuWidth: 320,
+			submenuHeight: 42,
+			submenuWidth: 153,
+			opacity: 0.8,
+			type: 'singleColumn',
+			alignment: 'left',
+			effect: 'none'
+		});
+	});
+	b.push(function () { x.utils.imPreloadImages([p + 'menu/hor_main.png',p + 'menu/hor_main_h.png',p + 'menu/hor_main_c.png',p + 'menu/ver_main.png',p + 'menu/ver_main_h.png',p + 'menu/ver_main_c.png', p + 'res/imLoad.gif', p + 'res/imClose.png']); });
+
+	// ShowBox
+	$.extend(s.imShowBox, {
+		'effect' : 'fade',
+		'shadow' : '',
+		'background' : '#000000',
+		'borderWidth' : {
+			'top': 1,
+			'right': 1,
+			'bottom': 1,
+			'left': 1
+		},
+		buttonRight: {
+			url: p + 'res/b01_r.png',
+			position: {
+				x: -12,
+				y: 0
+			}
+		},
+		buttonLeft: {
+			url: p + 'res/b01_l.png',
+			position: {
+				x: -12,
+				y: 0
+			}
+		},
+		'borderRadius' : '5px 5px 5px 5px',
+		'borderColor' : '#000000 #000000 #000000 #000000',
+		'textColor' : '#000000',
+		'fontFamily' : 'Tahoma',
+		'fontStyle' : 'normal',
+		'fontWeight' : 'normal',
+		'fontSize' : '9pt',
+		'textAlignment' : 'left',
+		'boxColor' : '#FFFFFF',
+		'opacity' : 0.7,
+		'radialBg' : true // Works only in Mozilla Firefox and Google Chrome
+	});
+
+	// PopUp
+	$.extend(s.imPopUp, {
+		'effect' : 'fade',
+		'width' : 500,
+		'shadow' : '',
+		'background' : '#000000',
+		'borderRadius' : 10,
+		'textColor' : '#000000',
+		'boxColor' : '#FFFFFF',
+		'opacity' : 0.7
+	});
+
+	// Tip
+	$.extend(s.imTip, {
+		'borderRadius' : 1,
+		'arrow' : true,
+		'position' : 'bottom',
+		'effect' : 'none',
+		'showTail' : true
+	});
+
+	// Captcha
+	b.push(function () {
+		x5engine.captcha.instance = new x5engine.captcha.x5captcha({
+			"phpfile": "captcha/x5captcha.php",
+		});
+	}, false, 'first');
+
+	// BreakPoints
+	s.breakPoints.push({ "hash": "3e39c63b4ae8247280e6e305de5ca2a8", "name": "Desktop", "start": "max", "end": 0, "fluid": false });
+
+	b.push(function () { x.cookielaw.activateScripts() });
+
+	s.loaded = true;
+})( _jq, x5engine );
